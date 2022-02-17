44824be30e550d71b00e889b4b9b4f3ff6bee7ae
skgitacc <sivakumarmuthu9797@gmail.com>
2022-02-17 13:24:56 +0530

    task

---
+++ src/org/test/Sshot.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class Sshot {
+
+	private void one() {
+		System.out.println("test 1");
+
+	}
+	private void two() {
+		System.out.println("test 2");
+
+	}
+	public static void main(String[] args) {
+		Sshot s = new Sshot();
+		s.one();
+		s.two();
+				
+	}
+}
